83b2a1b6c37c33125e221e3e947fa0e0cf177876
mstuazon <marie.sheridan.tuazon@gmail.com>
2016-02-17 01:56:43 +0800

    better animation

--- js/homepage.js
+++ js/homepage.js
@@ -4,12 +4,14 @@ $(document).ready(function(){
 
     hover.mouseenter(function(){
         $(this).css('color', '#862d59');
-        $(this).css('font-size', '1.5em');
+        //$(this).css('font-size', '1.5em');
+        $(this).animate({fontSize: '1.5em'}, 300);
     });
 
     hover.mouseleave(function(){
         $(this).css('color', '#c6538c');
-        $(this).css('font-size', '1em');
+        //$(this).css('font-size', '1em');
+        $(this).animate({fontSize: '1em'}, 300);
     });
 
 });
