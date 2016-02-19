0006d928ef24e5ac8212771ca0fdb02997e47cf2
mstuazon <marie.sheridan.tuazon@gmail.com>
2016-02-19 13:39:10 +0800

    Moved animation trigger to sectionwithtitle

--- js/homepage.js
+++ js/homepage.js
@@ -1,19 +1,42 @@
 
 $(document).ready(function(){
     var $hover = $('.hover-rise');
+    var $sectionWithLogo = $('.sectionwithlogo');
     var headerNameColor = $('#header-name').css('color');
     var newHeaderNameColor = "#ff1a8c";
     var headerOrigSize = $('#header-name').css('font-size');
     var sectionTitleOrigSize = $('.sectiontitle').css('font-size');
 
     $hover.mouseenter(function(){
-        $(this).css('color', newHeaderNameColor);
-        $(this).animate({fontSize: '+=0.5em' }, 300);
+        expandFont($(this), newHeaderNameColor);
     });
 
     $hover.mouseleave(function(){
-        var origSize = $(this).hasClass('sectiontitle') ? sectionTitleOrigSize : headerOrigSize;
-        $(this).css('color', headerNameColor);
-        $(this).animate({fontSize: origSize}, 250);
+        shrinkFont($(this), headerNameColor, headerOrigSize);
     });
+
+    $sectionWithLogo.mouseenter(function(){
+        $(this).find('.sectiontitle').each(function(){
+            expandFont($(this), newHeaderNameColor);
+        });
+    });
+
+    $sectionWithLogo.mouseleave(function(){
+        $(this).find('.sectiontitle').each(function(){
+            shrinkFont($(this), headerNameColor, sectionTitleOrigSize);
+        });
+    });
+
 });
+
+function expandFont($object, color)
+{
+    $object.css('color', color);
+    $object.animate({fontSize: '+=0.5em' }, 300);
+}
+
+function shrinkFont($object, color, origSize)
+{
+    $object.css('color', color);
+    $object.animate({fontSize: origSize}, 250);
+}
