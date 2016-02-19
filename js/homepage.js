9ba43c3e2c267c885ca8a8674c110fec5ed879e1
mstuazon <marie.sheridan.tuazon@gmail.com>
2016-02-19 11:24:13 +0800

    Animate sectiontitle

--- js/homepage.js
+++ js/homepage.js
@@ -1,18 +1,19 @@
 
 $(document).ready(function(){
-    var hover = $('.hover-rise');
+    var $hover = $('.hover-rise');
     var headerNameColor = $('#header-name').css('color');
     var newHeaderNameColor = "#ff1a8c";
+    var headerOrigSize = $('#header-name').css('font-size');
+    var sectionTitleOrigSize = $('.sectiontitle').css('font-size');
 
-    hover.mouseenter(function(){
-        
+    $hover.mouseenter(function(){
         $(this).css('color', newHeaderNameColor);
-        $(this).animate({fontSize: '1.5em'}, 300);
+        $(this).animate({fontSize: '+=0.5em' }, 300);
     });
 
-    hover.mouseleave(function(){
+    $hover.mouseleave(function(){
+        var origSize = $(this).hasClass('sectiontitle') ? sectionTitleOrigSize : headerOrigSize;
         $(this).css('color', headerNameColor);
-        $(this).animate({fontSize: '1em'}, 250);
+        $(this).animate({fontSize: origSize}, 250);
     });
-
 });
